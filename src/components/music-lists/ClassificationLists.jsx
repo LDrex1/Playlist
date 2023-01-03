3ff77b3de4e50a359b4bb51f524686b76fff95be
lDrex1 <damzealwini7@gmail.com>
2023-01-03 04:41:40 +0100

    new comp: Grid item containing mapped music lists

---
+++ src/components/music-lists/ClassificationLists.jsx
@@ -0,0 +1,51 @@
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import React from "react";
+import ClassificationList from "./ClassificationList";
+
+function ClassificationLists() {
+  const list = ["top artists", "billboard topcharts"];
+  return (
+    <Grid item md={7.75}>
+      {list.map((musicClass, index) => (
+        <Box
+          sx={{
+            background: "#333345",
+            borderRadius: 10,
+            display: "flex",
+            flexDirection: "column",
+            placeItems: "center",
+
+            px: 3.5,
+            pt: 2,
+            pb: 3,
+            marginBottom: index === list.length - 1 ? 0 : 4,
+          }}
+        >
+          <Stack
+            justifyContent={"space-between"}
+            sx={{ width: "100%" }}
+            direction={"row"}
+          >
+            <Typography
+              gutterBottom
+              textTransform={"capitalize"}
+              variant={"h6"}
+            >
+              {musicClass}
+            </Typography>
+            <Typography
+              gutterBottom
+              textTransform={"capitalize"}
+              variant={"caption"}
+            >
+              see all
+            </Typography>
+          </Stack>
+          <ClassificationList />
+        </Box>
+      ))}
+    </Grid>
+  );
+}
+
+export default ClassificationLists;
