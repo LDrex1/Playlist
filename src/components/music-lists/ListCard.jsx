21066959854b11df0d828d23115fb3693680ddbc
lDrex1 <damzealwini7@gmail.com>
2023-01-03 04:42:09 +0100

    new comp: Card containing artist image and info

---
+++ src/components/music-lists/ListCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+
+function ListCard(props) {
+  const { maxWidth, height, imgSrc, artist, secTypo } = props;
+  return (
+    <Card
+      sx={{
+        background: "none",
+        height: height,
+        maxWidth: maxWidth,
+        boxShadow: "none",
+      }}
+    >
+      <CardMedia
+        sx={{
+          borderRadius: 5,
+        }}
+        component={"img"}
+        alt={"img"}
+        src={imgSrc}
+      />
+      <CardContent color={"white"}>
+        <Typography
+          variant={"h6"}
+          textTransform={"capitalize"}
+          color={"white"}
+          textAlign={"center"}
+        >
+          {artist}
+        </Typography>
+        <Typography
+          component={"p"}
+          variant={"body2"}
+          textTransform={"capitalize"}
+          color={"white"}
+          textAlign={"center"}
+        >
+          {secTypo}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ListCard;
