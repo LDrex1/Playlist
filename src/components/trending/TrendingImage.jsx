5e1c55d64fd1f81d6755784e718e9f8b6148bd27
lDrex1 <damzealwini7@gmail.com>
2023-01-03 04:43:45 +0100

    update: card for the image in the trending section

--- src/components/trending/TrendingImage.jsx
+++ src/components/trending/TrendingImage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { CardMedia, Card } from "@mui/material";
+import React from "react";
+import omahLay from "../../assets/Artist-Omah-Lay.png";
 
 function TrendingImage() {
   return (
-    <></>
-  )
+    <Card
+      sx={{
+        position: "absolute",
+        right: "8%",
+        top: 0,
+        background: "none",
+        mixBlendMode: "color-dodge",
+      }}
+    >
+      <CardMedia
+        component={"img"}
+        alt={"artist"}
+        src={omahLay}
+        height={500}
+        width={400}
+      />
+    </Card>
+  );
 }
 
-export default TrendingImage
+export default TrendingImage;
