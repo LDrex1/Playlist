efe54e9833e12c991d1d5f838a687f4950fb2a9b
lDrex1 <damzealwini7@gmail.com>
2023-01-03 04:41:01 +0100

    new comp: flexbox where ListCard is mapped

---
+++ src/components/music-lists/ClassificationList.jsx
@@ -0,0 +1,24 @@
+import { Stack } from "@mui/system";
+import React from "react";
+import ListCard from "./ListCard";
+import omahLay from "../../assets/Artist-Omah-Lay.png";
+
+function ClassificationList() {
+  const list = [1, 2, 3, 4];
+  return (
+    <Stack direction={"row"} spacing={2} justifyContent={"center"}>
+      {list.map((music) => (
+        <ListCard
+          key={music}
+          height={201.25}
+          artist={"omah lay"}
+          imgSrc={omahLay}
+          secTypo={"8.5k plays"}
+          maxWidth={137.77}
+        />
+      ))}
+    </Stack>
+  );
+}
+
+export default ClassificationList;
