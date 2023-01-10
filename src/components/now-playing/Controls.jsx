e690f87842d2981cac0a1622b664601224d1f302
lDrex1 <damzealwini7@gmail.com>
2023-01-10 22:03:20 +0100

    added: control icons

--- src/components/now-playing/Controls.jsx
+++ src/components/now-playing/Controls.jsx
@@ -1,7 +1,27 @@
+import {
+  FirstPageOutlined,
+  PauseOutlined,
+  PlayArrowOutlined,
+  SkipNextOutlined,
+} from "@mui/icons-material";
+import { Box, IconButton } from "@mui/material";
 import React from "react";
 
 function Controls() {
-  return <div>Controls</div>;
+  return (
+    <Box sx={{ mt: -1.2 }}>
+      <IconButton aria-label="previous song">
+        <FirstPageOutlined />
+      </IconButton>
+      <IconButton aria-label={"pause"}>
+        <PlayArrowOutlined />
+        <PauseOutlined />
+      </IconButton>
+      <IconButton aria-label={"pause"}>
+        <SkipNextOutlined />
+      </IconButton>
+    </Box>
+  );
 }
 
 export default Controls;
