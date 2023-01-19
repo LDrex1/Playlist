c3d396f201fdf6bb6c4d501e3904837363816af7
lDrex1 <damzealwini7@gmail.com>
2023-01-19 10:44:21 +0100

    update: nothing changed

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Grid } from "@mui/material";
 import NavigationDrawer from "../../components/navigation/side-navigation/NavigationDrawer";
 import MainLandCont from "./MainLandCont";
+import { useEffect } from "react";
+import { createEntityAdapter } from "@reduxjs/toolkit";
+import client from "../../api/client";
+// import { fetchMusic } from "../../features/music/MusicSlice";
 
 function Landing() {
+  //   const dispatch = useDispatch();
+  //   const { music, status, error } = useSelector((state) => state.music);
+  //   useEffect(() => {
+  //     if (status === "idle") {
+  //       dispatch(fetchMusic());
+  //     }
+  //   }, [status, dispatch]);
   return (
     <Grid container justifyContent={"space-between"} alignItems={"stretch"}>
       <NavigationDrawer />
