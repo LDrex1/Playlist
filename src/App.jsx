cb2b27d0ab8aa90f1c5866ac488a9686ddeff35c
lDrex1 <damzealwini7@gmail.com>
2023-01-20 00:34:24 +0100

    added: condition for rendering SearchResult

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import TopNavigation from "./components/navigation/top-navigation/TopNavigation";
+import NavigationDrawer from "./components/navigation/side-navigation/NavigationDrawer";
+import MainLandCont from "./pages/landing/MainLandCont";
 import Landing from "./pages/landing/Landing";
+import SearchResult from "./pages/landing/SearchResult";
+import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <Box>
-      <TopNavigation />
-      <Landing />
+      <TopNavigation setSearch={setSearch} />
+      <Grid container justifyContent={"space-between"} alignItems={"stretch"}>
+        <NavigationDrawer />
+        <Grid item md={10.5}>
+          {search ? <SearchResult search={search} /> : <MainLandCont />}
+        </Grid>
+      </Grid>
+      {/* <Landing /> */}
     </Box>
   );
 }
