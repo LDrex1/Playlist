9b1a07f6d8b36338f1c5b89ad5ad3fef0ad0d3d1
lDrex1 <damzealwini7@gmail.com>
2023-01-19 10:44:49 +0100

    cleanup of unused imports

--- src/pages/landing/MusicContent.jsx
+++ src/pages/landing/MusicContent.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Grid } from "@mui/material";
 import ClassificationLists from "../../components/music-lists/ClassificationLists";
-import NavigationDrawer from "../../components/navigation/side-navigation/NavigationDrawer";
-import NowPlaying from "../../components/now-playing/NowPlaying";
 
 function MusicContent() {
   return <ClassificationLists />;
