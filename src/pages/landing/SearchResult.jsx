0ba79e5333ab5a7016e0e5cf04d2da2eee74dc58
lDrex1 <damzealwini7@gmail.com>
2023-01-20 00:36:51 +0100

    new comp: to render search results

---
+++ src/pages/landing/SearchResult.jsx
@@ -0,0 +1,45 @@
+import { Card, CardContent, CardMedia, Stack, Typography } from "@mui/material";
+import React from "react";
+import LongMusicCard from "../../components/music-lists/LongMusicCard";
+import { useSearchMusicQuery } from "../../features/api/spotifyApi";
+
+/**
+ *
+ * @param {search} the value of the search input
+ * @renders a list of the search results
+ */
+function SearchResult({ search }) {
+  const { data, isFetching, error } = useSearchMusicQuery({ search });
+  console.log(isFetching);
+  const tracks = data?.tracks;
+  return (
+    <>
+      {isFetching ? (
+        <div>loading</div>
+      ) : error ? (
+        <div>Error</div>
+      ) : (
+        <Stack mt={2} spacing={2.2}>
+          {tracks.map(({ data }) => {
+            const { artists, url, albumOfTrack, id, name, playability } = data;
+            const allArtists = artists.items;
+            const artistName = allArtists.reduce((acc, { profile }) => {
+              return acc + profile.name + " ";
+            }, "");
+            const [image] = albumOfTrack.coverArt.sources;
+            return (
+              <LongMusicCard
+                key={id}
+                name={name}
+                artistName={artistName}
+                image={image}
+              />
+            );
+          })}
+        </Stack>
+      )}
+    </>
+  );
+}
+
+export default SearchResult;
