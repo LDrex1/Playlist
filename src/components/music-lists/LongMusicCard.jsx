16fda572918c6a253a1f5d1911197af079f9e3fa
lDrex1 <damzealwini7@gmail.com>
2023-01-20 00:36:34 +0100

    new comp: resusable card for listing songs

---
+++ src/components/music-lists/LongMusicCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, CardContent, CardMedia, Stack, Typography } from "@mui/material";
+
+function LongMusicCard({ image, name, artistName }) {
+  return (
+    <Card sx={{ display: "flex", background: "#222222" }}>
+      <CardMedia
+        sx={{ width: 100, height: 100 }}
+        component={"img"}
+        alt="song_img"
+        image={image?.url}
+      />
+      <Stack sx={{}}>
+        <CardContent sx={{ flex: "1 0 auto", color: "#ffffff" }}>
+          <Typography component={"p"} variant={"p"}>
+            {name}
+          </Typography>
+          <Typography variant="subtitle1">{artistName}</Typography>
+        </CardContent>
+      </Stack>
+    </Card>
+  );
+}
+
+export default LongMusicCard;
