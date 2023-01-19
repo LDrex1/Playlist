6857bfd69d500a0ca2357c522b31092554660de7
lDrex1 <damzealwini7@gmail.com>
2023-01-20 00:40:55 +0100

    update: mapped lists

--- src/pages/landing/MusicContent.jsx
+++ src/pages/landing/MusicContent.jsx
@@ -1,8 +1,51 @@
 import React from "react";
-import ClassificationLists from "../../components/music-lists/ClassificationLists";
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import ClassificationList from "../../components/music-lists/ClassificationList";
 
 function MusicContent() {
-  return <ClassificationLists />;
+  const list = ["top artists", "billboard topcharts"];
+  return (
+    <Box>
+      {list.map((musicClass, index) => (
+        <Box
+          key={musicClass}
+          sx={{
+            background: "#333345",
+            borderRadius: 10,
+            display: "flex",
+            flexDirection: "column",
+            placeItems: "center",
+            px: 3.5,
+            pt: 2,
+            pb: 3,
+            marginBottom: index === list.length - 1 ? 0 : 4,
+          }}
+        >
+          <Stack
+            justifyContent={"space-between"}
+            sx={{ width: "100%" }}
+            direction={"row"}
+          >
+            <Typography
+              gutterBottom
+              textTransform={"capitalize"}
+              variant={"h6"}
+            >
+              {musicClass}
+            </Typography>
+            <Typography
+              gutterBottom
+              textTransform={"capitalize"}
+              variant={"caption"}
+            >
+              see all
+            </Typography>
+          </Stack>
+          <ClassificationList />
+        </Box>
+      ))}
+    </Box>
+  );
 }
 
 export default MusicContent;
