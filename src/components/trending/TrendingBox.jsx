4d26fcf374e0d9f7c71f0a66b8f1ba9fbb199e1b
lDrex1 <damzealwini7@gmail.com>
2023-01-26 22:36:47 +0100

    passed: name artistName and image prop

--- src/components/trending/TrendingBox.jsx
+++ src/components/trending/TrendingBox.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import TrendingImage from "./TrendingImage";
 import TrendingInfo from "./TrendingInfo";
 
-function TrendingBox() {
+function TrendingBox({ name, artistName, image }) {
   return (
     <Stack alignItems={"center"} sx={{ position: "relative", pt: 2 }}>
-      <TrendingInfo />
-      <TrendingImage />
+      <TrendingInfo name={name} artistName={artistName} />
+      <TrendingImage image={image} />
     </Stack>
   );
 }
