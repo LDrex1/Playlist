285ff2a0467eac2d345bc497ad38708185dbd944
lDrex1 <damzealwini7@gmail.com>
2023-03-17 02:59:57 +0100

    update: typography and CustomTab style

--- src/components/navigation/side-navigation/NavigationDrawer.jsx
+++ src/components/navigation/side-navigation/NavigationDrawer.jsx
@@ -4,15 +4,26 @@ import { listsSide } from "../NavigationData";
 import { CustomTab, CustomTabs } from "../../custom-mui/Tabs";
 function NavigationDrawer() {
   return (
-    <Grid item mt={5} md={1.5}>
+    <Grid item mt={5} md={1.2}>
       <Stack>
         <Box>
-          <Typography component={"p"} variant={"h6"}>
+          <Typography
+            textTransform={"uppercase"}
+            component={"p"}
+            variant={"h6"}
+          >
             Music
           </Typography>
           <CustomTabs orientation={"vertical"}>
             {listsSide.map(({ name, icon }) => (
               <CustomTab
+                sx={{
+                  paddingLeft: 0,
+                  paddingRight: 0,
+                  maxWidth: 20,
+                  minHeight: 50,
+                  justifyContent: "start",
+                }}
                 key={name}
                 icon={icon}
                 iconPosition={"start"}
