998fc160ed08bd804b411d903cff1f9c4189e29f
lDrex1 <damzealwini7@gmail.com>
2023-03-18 21:19:24 +0100

    added: side nav screen responsiveness

--- src/components/navigation/side-navigation/NavigationDrawer.jsx
+++ src/components/navigation/side-navigation/NavigationDrawer.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
 import { Box, Grid, Stack, Typography } from "@mui/material";
 import { listsSide } from "../NavigationData";
 import { CustomTab, CustomTabs } from "../../custom-mui/Tabs";
+import { size } from "../../devices";
 function NavigationDrawer() {
+  const [deviceSize, setDeviceSize] = useState(null);
+
+  const checkWindowWidth = () => {
+    setDeviceSize(() =>
+      innerWidth > +size.mobileL.match(/\d+/).join() ? "large" : "small"
+    );
+  };
+
+  useEffect(() => {
+    addEventListener("resize", checkWindowWidth);
+    addEventListener("loadstart", checkWindowWidth);
+    return () => {
+      removeEventListener("resize", checkWindowWidth);
+      removeEventListener("loadstart", checkWindowWidth);
+    };
+  }, [innerWidth]);
+
   return (
-    <Grid item mt={5} md={1.2}>
+    <Grid item mt={5} sm={1.2}>
       <Stack>
         <Box>
           <Typography
@@ -24,10 +43,12 @@ function NavigationDrawer() {
                   minHeight: 50,
                   justifyContent: "start",
                 }}
+                // component={Link}
                 key={name}
                 icon={icon}
+                // to="/"
                 iconPosition={"start"}
-                label={name}
+                label={deviceSize === "large" && name}
               />
             ))}
           </CustomTabs>
