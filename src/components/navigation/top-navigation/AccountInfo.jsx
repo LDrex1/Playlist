177e80c0810822c08accdf6f4f3130d86084280c
lDrex1 <damzealwini7@gmail.com>
2023-03-18 21:20:04 +0100

    added: state and elements for responsiveness

--- src/components/navigation/top-navigation/AccountInfo.jsx
+++ src/components/navigation/top-navigation/AccountInfo.jsx
@@ -1,43 +1,81 @@
-import React from "react";
-import { NotificationAdd, SettingsOutlined } from "@mui/icons-material";
-import { Badge, Card, CardMedia, IconButton, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  NotificationAdd,
+  SettingsOutlined,
+  MenuOutlined,
+} from "@mui/icons-material";
+import { Badge, Button, Card, CardMedia, IconButton } from "@mui/material";
 import { Stack } from "@mui/system";
+import device from "../../devices";
 
 function AccountInfo() {
+  const [openInfo, setOpenInfo] = useState(false);
+
   return (
-    <Stack direction={"row"} alignItems={"center"}>
-      <IconButton
-        size={"large"}
-        aria-label={"show 2 new notifications"}
-        color={"inherit"}
-      >
-        <Badge badgeContent={2} color={"error"}>
-          <NotificationAdd />
-        </Badge>
-      </IconButton>
-      <IconButton
-        size={"large"}
-        aria-label={"show 2 new notifications"}
-        color={"inherit"}
-      >
-        <Badge badgeContent={0} color={"error"}>
-          <SettingsOutlined />
-        </Badge>
-      </IconButton>
-      <Card
+    <>
+      <Button
         sx={{
-          width: 37,
-          aspectRatio: 1,
-          borderRadius: "50%",
-          background: "#222215",
+          color: "whitesmoke",
+          [`@media ${device.mobileL}`]: {
+            display: "none",
+          },
         }}
+        onClick={() => setOpenInfo((prev) => !prev)}
+        startIcon={<MenuOutlined />}
+      ></Button>
+      <Stack
+        sx={{
+          background: "#222222",
+          borderRadius: 5,
+          position: "absolute",
+          top: 40,
+          right: 0,
+          flexDirection: "column",
+          visibility: openInfo ? "visible" : "hidden",
+          [`@media ${device.mobileL}`]: {
+            position: "static",
+            background: "none",
+            flexDirection: "row",
+            visibility: "visible",
+          },
+        }}
+        rowGap={0.1}
+        direction={"row"}
+        alignItems={"center"}
       >
-        <CardMedia component={"img"} sec="" />
-      </Card>
-      <Typography component={"h3"} variant={"h6"}>
-        Mayowa Olorunfemi
-      </Typography>
-    </Stack>
+        <Button
+          sx={{ color: "whitesmoke" }}
+          endIcon={
+            <IconButton
+              size={"large"}
+              aria-label={"show 2 new notifications"}
+              color={"inherit"}
+            >
+              <Badge badgeContent={2} color={"error"}>
+                <NotificationAdd />
+              </Badge>
+            </IconButton>
+          }
+        >
+          Olorunfemi
+        </Button>
+        <IconButton color={"inherit"} size={"large"}>
+          <Badge badgeContent={0} color={"error"}>
+            <SettingsOutlined />
+          </Badge>
+        </IconButton>
+        <Card
+          sx={{
+            width: 37,
+            aspectRatio: 1,
+            borderRadius: "50%",
+            background: "#222215",
+          }}
+        >
+          <CardMedia component={"img"} sec="" />
+        </Card>
+      </Stack>
+    </>
   );
 }
 
